ae980b3d9de80be3520d544c044cc65bf1fbac7f
すー <aaaiiiuuu35@gmail.com>
2019-03-29 16:51:08 +0900

    formula

--- su/Source.cpp
+++ su/Source.cpp
@@ -19,7 +19,7 @@ void createStar(Matrix& matrix, size_t x, size_t y, size_t star_x, size_t star_y
     constexpr size_t stars = 5;
     constexpr double angle = 360.0 / (double)stars;
     constexpr double PI = 3.141592653589793;
-    int64_t vertex[stars][2] = {0};
+    size_t vertex[stars][2] = {0};
 
     vertex[0][0] = star_x;
     vertex[0][1] = star_y - star_r;
@@ -27,23 +27,26 @@ void createStar(Matrix& matrix, size_t x, size_t y, size_t star_x, size_t star_y
 
 
     for (size_t i = 1; i < stars; ++i) {
+        std::cout << "ang:" << angle * i << std::endl;
         double degree = (double)(i * angle * PI) / 180.0;
-        std::cout << degree << std::endl;
+        std::cout << "ƒÆ:" <<degree << std::endl;
+
+        auto cx = (vertex[i - 1][0] - star_x);
+        std::cout << "cx:" <<cx << std::endl;
+        auto cy = (vertex[i - 1][1] - star_y);
+        std::cout << "cy:" << cy << std::endl;
+
         vertex[i][0] = 
-              (int64_t)((vertex[i - 1][0] * std::cos(degree)))
-            - (int64_t)((vertex[i - 1][1] * std::sin(degree)))
-            + (int64_t)(star_x * (1 - std::cos(degree)))
-            + (int64_t)(star_y * std::sin(degree));
+              (size_t)((cx * std::cos(degree)))
+            - (size_t)((cy * std::sin(degree)));
         vertex[i][1] = 
-            (int64_t)((vertex[i - 1][0] * std::sin(degree)))
-            + (int64_t)((vertex[i - 1][1] * std::cos(degree)))
-            - (int64_t)(star_x * std::sin(degree))
-            + (int64_t)(star_y * (1 - std::cos(degree)));
+              (size_t)((cx * std::cos(degree)))
+            + (size_t)((cy * std::sin(degree)));
     }
 
     for (auto&& i : vertex) {
         std::cout << i[0] << ":" << i[1] << std::endl;
-        matrix[i[0]][i[1]] = (int64_t)value;
+        //matrix[i[0]][i[1]] = value;
     }
     
 }
